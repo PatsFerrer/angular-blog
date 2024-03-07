c4fec1a4e1cfadea29afef03ff701e3e90de006f
Patricia Ferrer <124654139+PatsFerrer@users.noreply.github.com>
2024-03-07 19:14:48 -0300

    feat: add 'Id' input property and mock data in dataFake.ts

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,32 @@
 export const dataFake = [
     {
-        "id": 1,
-        "photo": "link",
-        "title": "saiu novo filme",
-        "description": "ola"
+        "id": '1',
+        "photoCover": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/A480F1D7CA7E5A78B2E5560688851AD5DF3C603351D7620B91489203074E62E2/scale?width=506",
+        "title": "NOVO HOMEM DE FERRO",
+        "description": "Marvel anuncia novo Homem de Ferro."
     },
     {
-        "id": 2,
-        "photo": "link",
-        "title": "saiu novo filme",
-        "description": "ola"
-    }
+        "id": '2',
+        "photoCover": "https://tenhaseuingresso.com/wp-content/uploads/2024/02/quarteto-1.jpg",
+        "title": "NOVO QUARTETO FANTÁSTICO ANUNCIADO",
+        "description": "Quarteto Fantástico confirma Kirby, Joseph Quinn e Ebon Moss-Bachrach no elenco."
+    },
+    {
+        "id": '3',
+        "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg",
+        "title": "Nova série anunciada no Disney+",
+        "description": "blabl abala"
+    },
+    {
+        "id": '4',
+        "photoCover": "https://hypando.com.br/wp-content/uploads/2023/02/20230222_084457-scaled.jpg",
+        "title": "Daredevil: Born Again | Jon Bernthal irá retornar",
+        "description": ""
+    },
+    {
+        "id": '5',
+        "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2023/10/Deadpool-vs-Wolverine.jpg",
+        "title": "Deadpool e Wolverine aparecem em novas imagens",
+        "description": ""
+    },
 ]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { dataFake } from 'src/app/data/dataFake';
 
 @Component({
   selector: 'app-content',
@@ -8,12 +10,29 @@ import { Component, OnInit } from '@angular/core';
 export class ContentComponent implements OnInit {
 
   photoCover: string = 'https://artefinalhq.com.br/wp/wp-content/uploads/2022/01/Homem-De-Ferro-Robert-Downey2.jpeg'
-  contentTitle: string = 'NOTICIA EXEMPLO'
-  contentDescription: string = 'bla blabl  abla'
+  contentTitle: string = ''
+  contentDescription: string = ''
+  private id: string | null = '0'
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {
+
+  }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(value => this.id = value.get("id"))
+
+    this.setValuesToComponent(this.id)
   }
+
+  setValuesToComponent(id: string | null) {
+    const result = dataFake.filter(article => article.id === id)[0]
+
+    this.contentTitle = result.title
+    this.contentDescription = result.description
+    this.photoCover = result.photoCover
+  }
+
+  
+  
 
 }
